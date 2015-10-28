80c5f35b07de5255afe2fbf727c930c00401175d
Maxwell Dergosits <mdergosits@gmail.com>
2015-10-28 16:40:48 -0400

    fixed condition

--- conf.go
+++ conf.go
@@ -21,12 +21,14 @@ func ReadConf(filename string) * Conf {
   file_contents := string(bytes)
   lines := strings.Split(file_contents, "\n")
   for i,line := range lines {
-    if len(line) > 0 && line[0:1] == "#" {
+    if len(line) <= 0 || line[0:1] == "#" {
       continue
     }
+    log.Println("line:",line, len(line))
     contents := strings.SplitN(line, ":", 2)
     if (len(contents) < 2) {
       log.Println("line",i,"invalid:",line)
+      continue
     }
     config[contents[0]] = contents[1]
   }
