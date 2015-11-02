d8f0fbe925dfaafd5482c7082e0d0f1cec907bd1
Maxwell Dergosits <mdergosits@gmail.com>
2015-11-01 20:41:55 -0500

    removed some logging

--- map_template.go
+++ map_template.go
@@ -1,11 +1,9 @@
 package main
 import "strings"
 import "path/filepath"
-import "log"
 
 
 func RenderMap(r * RenderContext, match string, render func(*RenderContext, string)string) string {
-  log.Println("mapping:",match)
   content := strings.TrimPrefix(match, "map:")
   tokens := strings.SplitN(content, ":",2)
   if (len(tokens) <2) {
@@ -14,7 +12,6 @@ func RenderMap(r * RenderContext, match string, render func(*RenderContext, stri
   file := tokens[0]
   key := tokens[1]
   values := ReadMap(filepath.Join(r.Directory,file))
-  log.Println("key value=:",key,values[key])
   return values[key]
 }
 
